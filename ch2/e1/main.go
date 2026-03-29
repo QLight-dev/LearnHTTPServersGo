4bfd20716cc4583146a2deb846a0bfb18e3bb8f4
Muhammed Uzair <nayazmuhammed094@gmail.com>
2026-03-29 19:26:02 +0400

    feat(e1): complete e1

---
+++ ch2/e1/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareFileServerHitsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		cfg.fileServerHits.Add(1)
+		next.ServeHTTP(w, req)
+	})
+}
+func main() {
+	mux := http.NewServeMux()
+	var cfg apiConfig
+	mux.Handle("/app/", http.StripPrefix("/app", cfg.middlewareFileServerHitsInc(http.FileServer(http.Dir(".")))))
+	mux.HandleFunc("/metrics", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(w, "Hits: %d", cfg.fileServerHits.Load())
+		w.WriteHeader(http.StatusOK)
+	})
+	mux.HandleFunc("/reset", func(w http.ResponseWriter, req *http.Request) {
+		cfg.fileServerHits.Store(0)
+		w.WriteHeader(http.StatusOK)
+	})
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		io.WriteString(w, "OK")
+	})
+
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		panic(err)
+	}
+}
